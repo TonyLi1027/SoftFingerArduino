00bd06cc3d76fcf2d4070320f6b1671e2f23fcec
TonyTheScrub <35352418+TonyTheScrub@users.noreply.github.com>
2019-08-19 16:29:44 -0700

    Add files via upload

--- test.ino
+++ test.ino
@@ -1,5 +1,11 @@
 char c;
-String point;
+const size_t bufferSize = 10;
+char point[bufferSize];
+char test_array[] = "488, 271, 406, 314, 307, 340, 209, 344, 126, 324";
+int arr[bufferSize];
+char *ptr = NULL;
+byte i = 0;
+size_t index = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0,OUTPUT);
@@ -10,11 +16,20 @@ void loop() {
   // put your main code here, to run repeatedly:
   while(Serial.available()>0){
     c = Serial.read();
-    point += c;
+    point[index++] = c;
   }
-  if(point == "[(488, 271), (406, 314), (307, 340), (209, 344), (126, 324)]"){
+  ptr = strtok(test_array, ",");//used to be point
+
+  
+  while (ptr != NULL && i < bufferSize) {
+  arr[i++] = atoi(ptr);
+  ptr = strtok(NULL, ",");
+  }
+  
+  if(arr[1] == 271 && arr[0] == 498){
       digitalWrite(A0,HIGH);
       }else{
         digitalWrite(A0,LOW);
         }
+  
 }
