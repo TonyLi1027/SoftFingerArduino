6f3a3c41e0bdd6c62eaf3ec7b31d3bd09f7daa3b
TonyTheScrub <35352418+TonyTheScrub@users.noreply.github.com>
2021-02-28 12:46:21 -0800

    initial commit

---
+++ Pinput.ino
@@ -0,0 +1,16 @@
+#include <math.h>  
+int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
+                    // outside leads to ground and +5V
+int val = 0;  // variable to store the value read
+
+void setup() {
+  Serial.begin(9600);           //  setup serial
+}
+
+float M=1;
+void loop() {
+  val = analogRead(analogPin);  // read the input pin
+  Serial.println(val);          // debug value
+ delay(20);
+
+}
